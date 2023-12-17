a9590b98ae08439e6869c1955e5d126677678bc1
mareeswaran <mareeswaran.g@gmail.com>
2023-12-18 03:40:40 +0530

    commit on 18dec

--- components/sections/home2/About.js
+++ components/sections/home2/About.js
@@ -15,36 +15,19 @@ export default function About() {
                     <div className="row"> 
                         <div className="col-lg-6">
                             <div className="image-block">
-                                <div className="image-one"><img className="lazy-image owl-lazy" src="/assets/images/resource/home-one.png" alt="" /></div>
-                                <div className="image-two"><img className="lazy-image owl-lazy" src="/assets/images/resource/lab-person.png" style={{width:300}} alt="" /></div>
+                                <div className="image-one"><img className="lazy-image owl-lazy" src="/assets/images/resource/home-one.jpg" alt="" /></div>
+                                <div className="image-two"><img className="lazy-image owl-lazy" src="/assets/images/resource/lab-person.jpg" alt="" /></div>
                                 <div className="image-three">
-                                    <img className="lazy-image owl-lazy" src="/assets/images/resource/tablets.jpg" style={{width:300, height:290}} alt="" />
-                                    <div className="video-box">
-                                        <a onClick={() => setOpen(true)} className="overlay-link lightbox-image video-fancybox ripple"><span className="play-icon flaticon-play" />
-                                            <ReactCurvedText width='180'
-                                                height='180'
-                                                cx='90'
-                                                cy='90'
-                                                rx='60'
-                                                ry='60'
-                                                startOffset='0'
-                                                reversed={true}
-                                                text='World’s #1 Consultancy . Works where it matters'
-                                                textProps={{ "style": { "fontSize": "18" } }}
-                                                textPathProps={{ "fill": "#ffffff" }}
-                                                tspanProps={null}
-                                                ellipseProps={null}
-                                                svgProps={null}
-                                            />
-                                        </a>
-                                    </div>
+                                    <img className="lazy-image owl-lazy" src="/assets/images/resource/lab-dark.jpg" alt="" />
+                                    
                                 </div>
                             </div>
                         </div>
                         <div className="col-lg-6">
                             <div className="sec-title style-two">
                                 <h4><span className="flaticon-endless" />About Ceegolabs</h4>
-                                <h2>Our passion for excellence ensures the <br />highest quality of product</h2>
+                                {/* <h2>Our passion for excellence ensures the <br />highest quality of product</h2> */}
+                                <h2>Creating A Business With <br/> Authentic Integrity</h2>
 
                             </div> .
                             <div className="content-block">
@@ -54,7 +37,7 @@ export default function About() {
                             </div>
                             {/*Testimonial Carousel*/}
                             
-                            <Link href="#" className="read-more-link"><i className="flaticon-right-arrow" />More About Company</Link>
+                            <Link href="/about-3" className="read-more-link"><i className="flaticon-right-arrow" />More About Company</Link>
                         </div>
                     </div>
                 </div>
--- components/sections/home2/Services.js
+++ components/sections/home2/Services.js
@@ -11,7 +11,7 @@ export default function Services() {
                             <h4><span className="flaticon-endless" />What We Do</h4>
                             <h2>Our Services</h2>
                         </div>
-                        <div className="link-btn mb-50"><Link href="#" className="theme-btn btn-style-five"><span className="btn-title">All Services</span></Link></div>
+                        
                     </div>
                     <div className="row">
                         <div className="col-lg-4 col-md-6 service-block-one">
@@ -19,7 +19,7 @@ export default function Services() {
                                 <div className="icon"><div className="icon-inner"><span className="flaticon-up" /></div></div>
                                 <h4>Contract Manufacturing</h4>
                                 <div className="text">Lorem Ipsum is &amp; dummy text of the printing and typesetting industry.</div>
-                                <Link href="#" className="read-more-link"><i className="flaticon-right-arrow" />Read More</Link>
+                                <Link href="/contract-manufacturing" className="read-more-link"><i className="flaticon-right-arrow" />Read More</Link>
                             </div>
                         </div>
                         <div className="col-lg-4 col-md-6 service-block-one">
@@ -27,7 +27,7 @@ export default function Services() {
                                 <div className="icon"><div className="icon-inner"><span className="flaticon-chart" /></div></div>
                                 <h4>Quality Complaince</h4>
                                 <div className="text">Lorem Ipsum is &amp; dummy text of the printing and typesetting industry.</div>
-                                <Link href="#" className="read-more-link"><i className="flaticon-right-arrow" />Read More</Link>
+                                <Link href="/quality-complaince" className="read-more-link"><i className="flaticon-right-arrow" />Read More</Link>
                             </div>
                         </div>
                         <div className="col-lg-4 col-md-6 service-block-one">
@@ -35,7 +35,7 @@ export default function Services() {
                                 <div className="icon"><div className="icon-inner"><span className="flaticon-system" /></div></div>
                                 <h4>Private Labeling</h4>
                                 <div className="text">Lorem Ipsum is &amp; dummy text of the printing and typesetting industry.</div>
-                                <Link href="#" className="read-more-link"><i className="flaticon-right-arrow" />Read More</Link>
+                                <Link href="/private-labeling" className="read-more-link"><i className="flaticon-right-arrow" />Read More</Link>
                             </div>
                         </div>
                         <div className="col-lg-4 col-md-6 service-block-one">
@@ -43,7 +43,7 @@ export default function Services() {
                                 <div className="icon"><div className="icon-inner"><span className="flaticon-business-and-finance" /></div></div>
                                 <h4>Custom Packing</h4>
                                 <div className="text">Lorem Ipsum is &amp; dummy text of the printing and typesetting industry.</div>
-                                <Link href="#" className="read-more-link"><i className="flaticon-right-arrow" />Read More</Link>
+                                <Link href="/custom-packaging" className="read-more-link"><i className="flaticon-right-arrow" />Read More</Link>
                             </div>
                         </div>
                         <div className="col-lg-4 col-md-6 service-block-one">
@@ -51,7 +51,7 @@ export default function Services() {
                                 <div className="icon"><div className="icon-inner"><span className="flaticon-support" /></div></div>
                                 <h4>Distribution Support</h4>
                                 <div className="text">Lorem Ipsum is &amp; dummy text of the printing and typesetting industry.</div>
-                                <Link href="#" className="read-more-link"><i className="flaticon-right-arrow" />Read More</Link>
+                                <Link href="/distribution-support" className="read-more-link"><i className="flaticon-right-arrow" />Read More</Link>
                             </div>
                         </div>
                         <div className="col-lg-4 col-md-6 service-block-one">
@@ -59,7 +59,7 @@ export default function Services() {
                                 <div className="icon"><div className="icon-inner"><span className="flaticon-endless" /></div></div>
                                 <h4>New Formulation Development</h4>
                                 <div className="text">Lorem Ipsum is &amp; dummy text of the printing and typesetting industry.</div>
-                                <Link href="#" className="read-more-link"><i className="flaticon-right-arrow" />Read More</Link>
+                                <Link href="/new-formulation-development" className="read-more-link"><i className="flaticon-right-arrow" />Read More</Link>
                             </div>
                         </div>
                     </div>
