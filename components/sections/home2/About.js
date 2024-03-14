0d1caebdb28c2abdd371fb2bc68b64a3db8eebda
mareeswaran <mareeswaran.g@gmail.com>
2024-03-14 16:17:43 +0530

    fontchange

--- components/sections/home2/About.js
+++ components/sections/home2/About.js
@@ -2,7 +2,6 @@
 
 import Link from "next/link"
 import { useState } from 'react'
-import ReactCurvedText from 'react-curved-text'
 import ModalVideo from 'react-modal-video'
 
 
@@ -25,17 +24,17 @@ export default function About() {
                         </div>
                         <div className="col-lg-6">
                             <div className="sec-title style-two">
-                                <h4><span className="flaticon-endless" />About <span className="ceegotxt">Ceego Labs</span></h4>
-                                {/* <h2>Our passion for excellence ensures the <br />highest quality of product</h2> */}
+                                <h4><span className="flaticon-endless"></span>About <span className="ceegotxt">Ceego Labs</span></h4>
+                               
                                 <h2>Creating A Business With <br/> Authentic Integrity</h2>
 
-                            </div> .
+                            </div>
                             <div className="content-block">
                                 <div className="text"><span className="ceegotxtpara">Ceego Labs</span> provides state-of-the-art manufacturing facilities for Food Supplements, Pharmaceutical and Feed Supplement Formulations. We have good manufacturing practices and superior-quality products as a cardinal principle. Quality is paramount at <span className="ceegotxtpara">Ceego Labs</span>. Our quality check processes and procedures span every area of manufacturing, from raw materials to finished products. Our combination of highly experienced, well-qualified professionals and the latest technology allows us to be a pioneer in Pharmaceutical and Nutraceuticals manufacturing. We are dedicated to delivering the highest possible levels of customer satisfaction.</div>
                                 
                                
                             </div>
-                            {/*Testimonial Carousel*/}
+                             
                             
                             <Link href="/aboutus" className="read-more-link"><i className="flaticon-right-arrow" />More About Company</Link>
                         </div>
