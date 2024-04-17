eeff847325be6a9c0cc2a5b4c5be4c02459c1ea5
mareeswaran <mareeswaran.g@gmail.com>
2024-04-17 16:29:25 +0530

    a

--- app/new-formulation-development/page.js
+++ app/new-formulation-development/page.js
@@ -3,8 +3,8 @@ import AuditSlider1 from "@/components/slider/AuditSlider1";
 import Choose from "@/components/elements/Choose";
 import Link from "next/link";
 export const metadata = {
-  title:'New Formulation Development'
-}
+  title: "New Formulation Development",
+};
 export default function Home() {
   return (
     <>
@@ -24,14 +24,44 @@ export default function Home() {
                   <div className="row mb-5">
                     <div className="col-md-12">
                       <div className="text">
-                        
-                          
-                        Our Research and Development team is the key to the science behind our products. Our formulation development department is motivated to achieve improved bioavailability, efficacy, and efficiency. Utilizing the latest and newest formulas and radical innovation.
-                        
-                      </div>                      
+                        Our Research and Development team is the key to the
+                        science behind our products. Our formulation development
+                        department is motivated to achieve improved
+                        bioavailability, efficacy, and efficiency. Utilizing the
+                        latest and newest formulas and radical innovation. We provide excellent formulations for
+                      </div>
+                      <ul className="list">
+                        <li style={{fontSize:'15px'}}>
+                          <i className="fa fa-check" />
+                          Dietary supplements
+                        </li>
+                        <li style={{fontSize:'15px'}}>
+                          <i className="fa fa-check" />
+                          Food supplements
+                        </li>
+                        <li style={{fontSize:'15px'}}>
+                          <i className="fa fa-check" />
+                          ⁠Nutraceuticals
+                        </li>
+                        <li style={{fontSize:'15px'}}>
+                          <i className="fa fa-check" />
+                          Sports Nutrition
+                        </li>
+                        <li style={{fontSize:'15px'}}>
+                          <i className="fa fa-check" />
+                          ⁠Functional foods
+                        </li>
+                        <li style={{fontSize:'15px'}}>
+                          <i className="fa fa-check" />
+                          Pre Probiotics
+                        </li>
+                        <li style={{fontSize:'15px'}}>
+                          <i className="fa fa-check" />
+                          Pharmaceuticals
+                        </li>
+                      </ul>
                     </div>
-                  </div>                 
-                  
+                  </div>
                 </div>
                 <aside className="col-lg-4 d-none d-md-block">
                   <div className="service-sidebar">
@@ -70,9 +100,9 @@ export default function Home() {
                 </aside>
               </div>
               <div className="row">
-               <div className="col">
-                <Choose/>
-               </div>
+                <div className="col">
+                  <Choose />
+                </div>
               </div>
             </div>
           </section>
